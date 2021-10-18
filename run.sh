c9d8fc130e0975bbe60a48281b8a5c86c6b29de0
Simon Morozov <42973527+shiftuya@users.noreply.github.com>
2021-10-18 23:52:09 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 fastqc $1.fastq.gz
 mv $1_fastqc.html $1.html
 rm $1_fastqc.zip
-bwa index reference.fna
-bwa mem reference.fna $1.fastq.gz > $1.sam
+bwa index ref.fna
+bwa mem ref.fna $1.fastq.gz > $1.sam
 if (( $(echo  $(samtools flagstat $1.sam | grep -o -P '[0-9]*(\.[0-9]*)?(?=%)')">90" | bc -l) )); then
   echo "OK"
 else
